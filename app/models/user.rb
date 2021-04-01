42c5fb4ee2614a15324c8646df9eeb8cb6516031
noia-00 <owner@MA4-0036.elecom>
2021-04-01 15:47:27 +0900

    ビュー・CSSの配置、バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,           presence: true
+  validates :email,              presence: true
+  validates :encrypted_password, presence: { with: /\A[a-z0-9]+\z/i }
+  validates :lastname,           presence: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
+  validates :firstname,          presence: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
+  validates :lastname_kana,      presence: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :firstname_kana,     presence: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :date,               presence: true
 end
