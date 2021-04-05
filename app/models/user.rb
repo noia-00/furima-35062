a9366dee771ca827c6e8019cf4e1e231cc4c3b17
noia-00 <owner@MA4-0036.elecom>
2021-04-05 19:58:40 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :password, format: { with: /\A[a-z0-9]+\z/i }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   with_options presence: true do
     validates :nickname
     validates :lastname,           format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :user do
     nickname              { 'sampleuser' }
     email                 { 'sample@sample.com' }
-    password              { '123456' }
-    password_confirmation { '123456' }
+    password              { 'abc123' }
+    password_confirmation { 'abc123' }
     lastname              { '山田' }
     firstname             { '太郎' }
     lastname_kana         { 'ヤマダ' }
