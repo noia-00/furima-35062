2d625e187666b54cad7b92cedc6147c599d33fbd
noia-00 <owner@MA4-0036.elecom>
2021-04-06 15:13:18 +0900

    バリデーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,6 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :delivery_schedule
 
+  validates :name, :info, :price, presence: true
+  validates :category_id, :status_id, :shipping_fee_id, :prefecture_id, :delivery_schedule_id, numericality: { other_than: 1 } 
 end
