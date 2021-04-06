352080b513c3878ff0efdd7830876f53fe802e26
noia-00 <owner@MA4-0036.elecom>
2021-04-06 17:18:28 +0900

    販売手数料等の計算・表示

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -3,8 +3,11 @@ window.addEventListener('load', () => {
 
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
-  })
-  const addTaxDom = document.getElementById("add-tax-price");
-  addTaxDom.innerHTML = "入力した金額をもとに販売手数料を計算する処理"
+    const inputValue = priceInput.value; 
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+    const addProfitDom = document.getElementById("profit");
+    addProfitDom.innerHTML = Math.floor(inputValue - addTaxDom.innerHTML);         
+  });
+
 });
