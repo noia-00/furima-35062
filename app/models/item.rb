7e8cccb69645d92df99c134e1fe6f45986f1727d
noia-00 <owner@MA4-0036.elecom>
2021-04-06 18:21:07 +0900

    命名規則に則るよう変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,9 +5,9 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
-  belongs_to :shipping_fee
+  belongs_to :shippingfee
   belongs_to :prefecture
-  belongs_to :delivery_schedule
+  belongs_to :deliveryschedule
 
   validates :name, :info, :price, presence: true
   validates :category_id, :status_id, :shipping_fee_id, :prefecture_id, :delivery_schedule_id, numericality: { other_than: 1 } 
