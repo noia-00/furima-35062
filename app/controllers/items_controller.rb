faa037945366266fcfcd5b8be6043a8446675119
noia-00 <owner@MA4-0036.elecom>
2021-04-06 15:31:29 +0900

    コントローラー編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,6 +6,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name,:info,:category_id, :status_id, :shipping_fee_id, :prefecture_id, :delivery_schedule_id, :price).merge(user_id: current_user.id)
+  end
 end
