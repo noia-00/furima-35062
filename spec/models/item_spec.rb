318cf94a85b92074d5066cd10f43c9b29587b199
noia-00 <owner@MA4-0036.elecom>
2021-04-07 15:49:11 +0900

    続・修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -18,7 +18,7 @@ describe Item do
       it '商品画像がない場合は出品できないこと' do
         @item.image = nil
         @item.valid?
-        expect(@item.errors[:image]).to include
+        expect(@item.errors[:image]).to include("can't be blank")
       end
 
       it 'nameがない場合は登録できないこと' do
@@ -36,49 +36,55 @@ describe Item do
       it 'category_idが1だと登録できないこと' do
         @item.category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
 
       it 'status_idが1だと登録できないこと' do
         @item.status_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include
+        expect(@item.errors.full_messages).to include("Status must be other than 1")
       end
 
       it 'shipping_fee_idが1だと登録できないこと' do
         @item.shipping_fee_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include
+        expect(@item.errors.full_messages).to include("Shipping fee must be other than 1")
       end
 
       it 'prefecture_idが1だと登録できないこと' do
         @item.prefecture_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
       end
 
       it 'delivery_schedule_idが1だと登録できないこと' do
         @item.delivery_schedule_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include
+        expect(@item.errors.full_messages).to include("Delivery schedule must be other than 1")
       end
 
       it 'priceがない場合は登録できないこと' do
         @item.price = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include
+        expect(@item.errors.full_messages).to include("Price can't be blank")
       end
 
-      it 'priceが¥300~¥9,999,999の範囲外では登録できないこと' do
+      it 'priceが¥300未満では登録できないこと' do
         @item.price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+
+      it 'priceが¥9,999,999より上では登録できないこと' do
+        @item.price = 100000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
 
       it 'priceが全角では登録できないこと' do
         @item.price = '１０００'
         @item.valid?
-        expect(@item.errors.full_messages).to include
+        expect(@item.errors.full_messages).to include("Price is not a number")
       end
     end
   end
