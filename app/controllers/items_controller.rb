270d363570127060fc66c03d7ae1bb497e9d9620
noia-00 <owner@MA4-0036.elecom>
2021-04-09 12:10:32 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit]
   before_action :shild, only: :edit 
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
   
   def update
-    @item = Item.find(params[:id])
     @item.update(item_params)
     if @item.save
       redirect_to item_path(@item.id)
@@ -48,6 +46,10 @@ class ItemsController < ApplicationController
     @sellers = current_user.items
     @seller = @sellers.find_by(id: params[:id])
     redirect_to root_path unless @seller
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
